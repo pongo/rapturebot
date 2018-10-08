b90cc094947c42986414303c245405aeaca17564
Pavel Shakhov <ab.pongo@gmail.com>
2018-10-08 13:19:35 +0300

    Добавил run_async. Оба метода запускаются по расписанию.

--- src/modules/jobs.py
+++ src/modules/jobs.py
@@ -3,6 +3,8 @@
 import telegram
 from datetime import datetime
 
+from telegram.ext import run_async
+
 from src.config import CONFIG
 from src.modules.dayof.day_manager import DayOfManager
 from src.modules.models.leave_collector import LeaveCollector
@@ -11,6 +13,7 @@ from src.modules.weather import send_alert_if_full_moon
 from src.utils.cache import pure_cache, FEW_DAYS
 
 
+@run_async
 def daily_midnight(bot: telegram.Bot, job):
     # особый режим сегодняшнего дня
     DayOfManager.midnight(bot)
@@ -28,6 +31,7 @@ def daily_midnight(bot: telegram.Bot, job):
     for cid in CONFIG["weekly_stats_chats_ids"]:
         ReplyDumper.dump(cid)
 
+@run_async
 def daily_afternoon(bot: telegram.Bot, job):
     DayOfManager.afternoon(bot)
 

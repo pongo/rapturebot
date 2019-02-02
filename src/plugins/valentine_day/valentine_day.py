6d8e71c88434155bc5b3a80fc701beee84704989
Pavel Shakhov <ab.pongo@gmail.com>
2019-02-03 01:00:19 +0300

    Мне в личку отправляет завершающую стату по всем чатам

--- src/plugins/valentine_day/valentine_day.py
+++ src/plugins/valentine_day/valentine_day.py
@@ -1,16 +1,28 @@
 import telegram
 from telegram.ext import run_async
 
-from src.plugins.valentine_day.date_checker import is_day_active
+from src.config import CONFIG
+from src.modules.dayof.helper import set_today_special
+from src.plugins.valentine_day.date_checker import is_day_active, is_today_ending
 from src.plugins.valentine_day.handlers.card_handlers import revn_button_click_handler, \
     mig_button_click_handler, about_button_click_handler
 from src.plugins.valentine_day.handlers.draft_handlers import draft_heart_button_click_handler, \
     draft_chat_button_click_handler, private_text_handler
+from src.plugins.valentine_day.handlers.stats_redis import StatsRedis
 from src.plugins.valentine_day.model import MODULE_NAME, \
     DraftHeartButton, DraftChatButton, \
-    RevnButton, MigButton, AboutButton
+    RevnButton, MigButton, AboutButton, CACHE_PREFIX, StatsHumanReporter
+from src.utils.cache import cache, TWO_DAYS
 
 HTML = telegram.ParseMode.HTML
+
+
+def send_end(bot: telegram.Bot) -> None:
+    if not cache.get(f'{CACHE_PREFIX}:end:admin', False):
+        with StatsRedis() as stats:
+            text = StatsHumanReporter(stats).get_text(None)
+        bot.send_message(CONFIG.get('debug_uid', None), text, parse_mode=telegram.ParseMode.HTML)
+        cache.set(f'{CACHE_PREFIX}:end:admin', True, time=TWO_DAYS)
 
 
 class ValentineDay:
@@ -21,6 +33,18 @@ class ValentineDay:
         MigButton.CALLBACK_NAME: mig_button_click_handler,
         AboutButton.CALLBACK_NAME: about_button_click_handler
     }
+
+    @classmethod
+    def midnight(cls, bot: telegram.Bot) -> None:
+        """
+        Показывает ночные приветственное и подводящее итоги сообщения
+        """
+        if is_day_active():
+            set_today_special()
+            # DayBegin.send(bot)
+        if is_today_ending():
+            send_end(bot)
+
 
     @classmethod
     @run_async

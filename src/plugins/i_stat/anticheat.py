5e6180c40a552b266fa6ddca96674e4985c2c60c
Pavel Shakhov <ab.pongo@gmail.com>
2019-04-08 13:11:28 +0300

    Ослабил античит /i

--- src/plugins/i_stat/anticheat.py
+++ src/plugins/i_stat/anticheat.py
@@ -9,7 +9,7 @@ def cheats_found(chat_id: int, user_id: int, sum_count: int) -> bool:
     key = cheats_key(chat_id, user_id)
     sums = cache.get(key, 0)
     sums += sum_count
-    if sums > 20:
+    if sums > 50:
         return True
 
     cache.set(key, sums, time=10 * 60)  # 10m

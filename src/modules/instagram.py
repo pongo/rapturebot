48bd4630f49d53f0a06cfbdc504d360e8ece8c0d
Pavel Shakhov <ab.pongo@gmail.com>
2024-10-02 13:06:25 +0300

    Загрузка сторис инсты

--- src/modules/instagram.py
+++ src/modules/instagram.py
@@ -22,6 +22,7 @@ CACHE_PREFIX = 'instagram'
 MODULE_NAME = CACHE_PREFIX
 callback_upload_video = 'instagram_upload_video'
 re_instagram_url = re.compile(r"instagram\.com\S*?\/(?:p|tv|reel)\/([\w-]+)\/?")
+re_instagram_story = re.compile(r"instagram.com/stories/(?:\S+)/(\d+)/?")
 instagram_user = CONFIG.get('instagram_user')
 SEND_VIDEO_SIZE_LIMIT = 50 * 1048576  # 50mb https://core.telegram.org/bots/api#sendvideo
 
@@ -68,21 +69,56 @@ def get_first_instagram_post_id_from_message(message: telegram.Message):
     return None
 
 
+def get_first_instagram_story_id_from_message(message: telegram.Message):
+    stories = [
+        parse_instagram_story_id(n)
+        for n in message.parse_entities([MessageEntity.URL]).values()
+    ]
+    for story in stories:
+        if story is not None:
+            return story[0], story[1]
+    return None
+
+
+def parse_instagram_post_id(text: str):
+    m = re_instagram_url.search(text)
+    try:
+        return (m.group(1), text) if m else None
+    except Exception as _:
+        return None
+
+
+def parse_instagram_story_id(text: str):
+    m = re_instagram_story.search(text)
+    try:
+        return (m.group(1), text) if m else None
+    except Exception as _:
+        return None
+
+
 def process_message_for_instagram(message: telegram.Message) -> bool:
     if not instaloader_session_exists or instagram_user is None:
         return False
+
     post = get_first_instagram_post_id_from_message(message)
-    if post is None:
-        return False
-    post_id, url = post
-    call(message, post_id, url)
-    return True
+    if post is not None:
+        post_id, url = post
+        call(message, post_id, url)
+        return True
+
+    story = get_first_instagram_story_id_from_message(message)
+    if story is not None:
+        story_id, url = story
+        call(message, story_id, url, story=True)
+        return True
+
+    return False
 
 
-def call(message: telegram.Message, post_id: str, url: str):
+def call(message: telegram.Message, post_id: str, url: str, story=False):
     try:
         message.chat.send_action(action=ChatAction.UPLOAD_PHOTO)
-        images, videos = fetch_post(post_id, url)
+        images, videos = fetch_post(post_id, url, story)
         # is_private = message.chat.id >= 0
 
         if len(images) == 0 and len(videos) == 0:
@@ -182,7 +218,8 @@ def get_video_wh(video_path: str) -> Tuple[Optional[int], Optional[int]]:
         logger.error(e)
         return None, None
 
-    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'),
+                        None)
     if video_stream is None:
         logger.error('No video stream found')
         return None, None
@@ -198,9 +235,10 @@ def send_video_upload(bot: telegram.Bot, chat_id, message_id, video_url: str) ->
         }) as r:
             if not r.ok:
                 logger.info(f"Failed to download video ({r.status_code}) {video_url}")
-                bot.send_message(chat_id,
-                                 f"""Не смог скачать видео. Качайте сами:\n\n<a href="{video_url}">Video</a>""",
-                                 parse_mode=ParseMode.HTML, reply_to_message_id=message_id)
+                bot.send_message(
+                    chat_id,
+                    f"""Не смог скачать видео. Качайте сами:\n\n<a href="{video_url}">Video</a>""",
+                    parse_mode=ParseMode.HTML, reply_to_message_id=message_id)
                 return
             file_size = int(r.headers.get('Content-length', 0))
             # if file_size == 0:
@@ -208,6 +246,7 @@ def send_video_upload(bot: telegram.Bot, chat_id, message_id, video_url: str) ->
             # return
             if file_size >= SEND_VIDEO_SIZE_LIMIT:
                 bot.send_message(
+                    chat_id,
                     f"""Телеграм не дает отправить видео больше 50 мб. Качайте сами:\n\n<a href="{video_url}">Video</a>""",
                     parse_mode=ParseMode.HTML, reply_to_message_id=message_id)
                 return
@@ -220,20 +259,14 @@ def send_video_upload(bot: telegram.Bot, chat_id, message_id, video_url: str) ->
             # logger.info(filesize)
             if filesize >= SEND_VIDEO_SIZE_LIMIT:
                 bot.send_message(
+                    chat_id,
                     f"""Телеграм не дает отправить видео больше 50 мб. Качайте сами:\n\n<a href="{video_url}">Video</a>""",
                     parse_mode=ParseMode.HTML, reply_to_message_id=message_id)
                 return
 
         width, height = get_video_wh(f.name)
-        bot.send_video(chat_id, video=open(f.name, "rb"), reply_to_message_id=message_id, width=width, height=height)
-
-
-def parse_instagram_post_id(text: str):
-    m = re_instagram_url.search(text)
-    try:
-        return (m.group(1), text) if m else None
-    except Exception as _:
-        return None
+        bot.send_video(chat_id, video=open(f.name, "rb"), reply_to_message_id=message_id,
+                       width=width, height=height)
 
 
 def fetch_via_instaloader(post_id: str):
@@ -261,9 +294,13 @@ def fetch_via_instaloader(post_id: str):
         return None
 
 
-def fetch_via_our_vision_api(post_id: str, url: str):
-    r = requests.post(f'http://localhost:3000/api/v1/instagram',
-                      json={"post_id": post_id, "url": url})
+def fetch_via_our_vision_api(post_id: str, url: str, story=False):
+    if story:
+        r = requests.post(f'http://localhost:3000/api/v1/instagram_story',
+                          json={"id": post_id, "url": url})
+    else:
+        r = requests.post(f'http://localhost:3000/api/v1/instagram',
+                          json={"post_id": post_id, "url": url})
     res = r.json()
     if not res['ok']:
         logger.error(res)
@@ -281,14 +318,14 @@ def fetch_via_our_vision_api(post_id: str, url: str):
     return images, videos
 
 
-def fetch_post(post_id: str, url: str):
+def fetch_post(post_id: str, url: str, story=False):
     # return [], ["https://download.samplelib.com/mp4/sample-5s.mp4"]
 
     # result = fetch_via_instaloader(post_id)
     # if result is not None:
     #    return result
 
-    result = fetch_via_our_vision_api(post_id, url)
+    result = fetch_via_our_vision_api(post_id, url, story)
     if result is not None:
         return result
 

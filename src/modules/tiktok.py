80989e2304643874cb13a774abb43ade1e4908ff
Pavel Shakhov <ab.pongo@gmail.com>
2024-10-02 13:06:01 +0300

    Рефакторинг загрузки тиктоков

--- src/modules/tiktok.py
+++ src/modules/tiktok.py
@@ -1,13 +1,9 @@
-import html
-import json
 import re
 import os
 import shutil
-from typing import Union
 
 import requests
 import telegram
-from jsonpath_ng import parse
 from telegram import MessageEntity, ChatAction
 
 from src.utils.logger_helpers import get_logger
@@ -41,81 +37,73 @@ def process_message_for_tiktok(message: telegram.Message) -> bool:
 def call(message: telegram.Message, url: str):
     try:
         message.chat.send_action(action=ChatAction.UPLOAD_VIDEO)
-        res = requests.post(f'http://localhost:3000/api/v1/tiktok-video', json={"video": url})
-        if not res.ok:
-            logger.error("Failed to request from TikBot API: %s" % res.status_code)
+
+        videos = fetch_api(url)
+        if not videos:
+            logger.error(f"Tiktok api returns None for {url}")
+            send_vxtiktok(message, url)
             return
 
-        item_infos = res.json()
-        fetch_key = lambda key: parse("$..%s" % key).find(item_infos)[0].value
+        too_big = False
+        for video_url in videos:
+            r = send_video(message, video_url)
+            if r["ok"]:
+                logger.info(f"Processed tiktok {url}")
+                return
+            if r["too_big"]:
+                logger.info(f"[inside] too_big tiktok {url}")
+            if not too_big and r["too_big"]:
+                too_big = True
 
-        video_url = fetch_key("videoUrl")
-        #logger.info(video_url)
-        if len(video_url) == 0:
-            logger.error("Failed to find videoUrl in video meta: %s" % json.dumps(item_infos))
-            message.reply_html(
-                f"Could not download video \U0001f613, TikTok gave a bad video meta response \U0001f97a")
-            return
-
-        with CustomNamedTemporaryFile(suffix='.mp4') as f:
-            tmp_url = video_url #.replace('https://v1.musicaldown.com/dl?url=', 'https://muscdn.xyz/dl?url=')
-            with requests.get(tmp_url, stream=True, headers=item_infos.get("headers", { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0' })) as r:
-                if not r.ok:
-                    logger.info(f"Failed to download video {item_infos}")
-                    message.reply_html(f"""Could not download video \U0001f613, TikTok gave a bad response \U0001f97a ({r.status_code}). <a href="{video_url}">Video</a>""")
-                    return
-                #logger.info(r.headers)
-                file_size = int(r.headers.get('Content-length', 0))
-                #if file_size == 0:
-                    #message.reply_html(f"<a href='{video_url}'>Video</a>")
-                    #return
-                if file_size >= SEND_VIDEO_SIZE_LIMIT:
-                    message.reply_html(f"Телеграм не дает отправить видео больше 50 мб. Качайте сами:\n\n{video_url}")
-                    return
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
-                
-            with open(f.name, "rb") as infile:
-                infile.seek(0, os.SEEK_END)
-                filesize = infile.tell()
-                #logger.info(filesize)
-                if filesize >= SEND_VIDEO_SIZE_LIMIT:
-                    message.reply_html(f"Телеграм не дает отправить видео больше 50 мб. Качайте сами:\n\n{video_url}")
-                    return
-
-            message.reply_video(
-                video=open(f.name, "rb"),
-                disable_notification=True,
-                caption=build_caption(fetch_key),
-                parse_mode=telegram.ParseMode.HTML,
-            )
-            logger.info(f"Processed tiktok {url}")
+        if too_big:
+            logger.info(f"Processed too_big tiktok {url}")
+            message.reply_html(f"""Телеграм не дает отправить видео больше 50 мб. Качайте сами:\n\n<a href="{videos[0]}">Video</a>""")
+        else:
+            logger.info(f"Processed VX tiktok {url}")
+            send_vxtiktok(message, url)
     except Exception as e:
         logger.error("Failed to download tiktok %s: %s" % (url, repr(e)))
         logger.error(e)
 
 
-def build_caption(fetch_key):
+def send_vxtiktok(message: telegram.Message, url: str):
+    message.reply_text(url.replace("tiktok.com", "vxtiktok.com"))
+
+
+def send_video(message: telegram.Message, video_url: str):
     try:
-        video_caption = fetch_key("text")
-        if "#" in video_caption:
-            video_caption = video_caption.split("#")[0]
+        with CustomNamedTemporaryFile(suffix='.mp4') as f:
+            with requests.get(video_url, stream=True, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0'}) as r:
+                if not r.ok:
+                    logger.info(f"Failed to download video ({r.status_code}) {video_url}")
+                    return {"ok": False, "cant_download": True, "too_big": False}
 
-        likes = space_thousand(int(fetch_key("diggCount") or 0))
-        comments = space_thousand(int(fetch_key("commentCount") or 0))
-        plays = space_thousand(int(fetch_key("playCount") or 0))
+                file_size = int(r.headers.get('Content-length', 0))
+                if file_size >= SEND_VIDEO_SIZE_LIMIT:
+                    return {"ok": False, "cant_download": False, "too_big": True}
 
-        video_caption = html.escape(video_caption.strip())
-        for mention in fetch_key("mentions"):
-            video_caption = video_caption.replace(mention, f"<a href='https://tiktok.com/{mention}'>{mention}</a>")
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
 
-        return f"{video_caption}\n\n❤ {likes}\n💬 {comments}\n⏯ {plays}"
-    except:
-        return ''
+            with open(f.name, "rb") as infile:
+                infile.seek(0, os.SEEK_END)
+                file_size = infile.tell()
+                if file_size >= SEND_VIDEO_SIZE_LIMIT:
+                    return {"ok": False, "cant_download": False, "too_big": True}
+
+            message.reply_video(video=open(f.name, "rb"))
+            return {"ok": True}
+    except Exception as e:
+        logger.error("Failed to download tiktok: %s" % (repr(e)))
+        logger.error(e)
+        return {"ok": False, "cant_download": True, "too_big": False}
 
 
-def space_thousand(num: Union[int, float]):
-    """
-    https://stackoverflow.com/a/18891054/136559
-    """
-    return '{:,}'.format(num).replace(',', ' ')
+def fetch_api(url: str):
+    r = requests.post(f'http://localhost:3000/api/v1/tiktok-video', json={"video": url})
+    res = r.json()
+    if not res['ok']:
+        logger.error(res)
+        return None
+
+    return res["value"]["videos"]

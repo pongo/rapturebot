8f4a98bde4f8110fa7145bec9af0b1c3e7327f0b
Pavel Shakhov <ab.pongo@gmail.com>
2024-11-11 11:37:39 +0300

    В PTB 12 по-умолчанию спользуется UTC, а не локальное время сервера. Так что добавил указание временной зоны

--- src/bot_start/add_jobs.py
+++ src/bot_start/add_jobs.py
@@ -1,4 +1,5 @@
 from datetime import time
+from zoneinfo import ZoneInfo
 
 from src.modules.weeklystat import weekly_stats
 from src.modules.jobs import daily_midnight, daily_afternoon, every_hour
@@ -7,20 +8,20 @@ from src.modules.jobs import daily_midnight, daily_afternoon, every_hour
 def add_jobs(updater):
     updater.job_queue.run_daily(
         weekly_stats,
-        time=time(0, 0, 10),  # во сколько постим
+        time=time(0, 0, 10, tzinfo=ZoneInfo('Europe/Moscow')),  # во сколько постим
         days=(0,)  # постим в понедельник
     )
 
     # каждый день в 00:00
     updater.job_queue.run_daily(
         daily_midnight,
-        time=time(0, 0, 10),
+        time=time(0, 0, 10, tzinfo=ZoneInfo('Europe/Moscow')),
     )
 
     # каждый день в 12:00
     updater.job_queue.run_daily(
         daily_afternoon,
-        time=time(12, 0, 10),
+        time=time(12, 0, 10, tzinfo=ZoneInfo('Europe/Moscow')),
     )
 
     updater.job_queue.run_repeating(

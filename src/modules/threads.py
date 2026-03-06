7e3f8f4555fd2aac8d58bb859c3a941b2f9c872e
Pavel Shakhov <ab.pongo@gmail.com>
2026-03-06 13:27:28 +0300

    Починил отправку медиа из тредс

--- src/modules/threads.py
+++ src/modules/threads.py
@@ -8,7 +8,7 @@ from src.utils.logger_helpers import get_logger
 from src.utils.send_video_helpers import send_images, send_videos
 
 logger = get_logger(__name__)
-re_threads_url = re.compile(r"^https://(www)\.threads\.net/t/.+$")
+re_threads_url = re.compile(r"^https://www\.threads\.com/@[\w.-_]+/post/[\w\-_]+")
 CACHE_PREFIX = 'threads'
 MODULE_NAME = CACHE_PREFIX
 
@@ -32,28 +32,19 @@ def process_message_for_threads(message: telegram.Message) -> bool:
 
 def call(message: telegram.Message, url: str):
     try:
-        message.chat.send_action(action=ChatAction.UPLOAD_VIDEO)
+        message.chat.send_action(action=ChatAction.UPLOAD_PHOTO)
         r = fetch_post(url)
         if r is None:
-            return None
+            logger.error(f"Third threads api returns None for {url}")
+            return
 
-        caption, likes, replies, images, videos = r
-        text = build_text(caption, likes, replies)
-
-        if len(images) == 0 and len(videos) == 0:
-            message.reply_text(text)
-        else:
-            caption_sent = send_images(message, images, text)
-            send_videos(message, videos, text, caption_sent)
-
+        images, videos = r
+        send_images(message, images)
+        send_videos(message, videos)
         logger.info(f"Processed threads {url}")
     except Exception as e:
-        logger.error(f"Failed to download threads {url}: {repr(e)}")
+        logger.error("Failed to download threads %s: %s" % (url, repr(e)))
         logger.error(e)
-
-
-def build_text(caption, likes, replies):
-    return f"{caption}\n\n{likes} likes, {replies} replies"
 
 
 def fetch_post(url):
@@ -63,9 +54,9 @@ def fetch_post(url):
         logger.error(res)
         return None
 
-    caption = res["value"]["caption"]
-    likes = res["value"]["likes"]
-    replies = res["value"]["replies"]
     videos = res["value"]["videos"]
     images = res["value"]["images"]
-    return caption, likes, replies, images, videos
+    if len(images) == 0 and len(videos) == 0:
+        logger.error("Failed to download threads %s: empty result" % url)
+        return None
+    return images, videos
